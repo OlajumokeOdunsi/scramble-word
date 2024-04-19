273ebb505659609f74abf05a9073efbebc52f52d
Jum-jum-maker <80886533+Jum-jum-maker@users.noreply.github.com>
2024-04-20 00:57:15 +0100

    hi scramble

---
+++ index.js
@@ -0,0 +1,13 @@
+const content = document.getElementById('content');
+console.log(content)
+const wordtoRedact = document.getElementById("text");
+const button = document.getElementById('button')
+
+let redactContent=()=>{
+    const contentValue= content.value;
+    console.log(contentValue)
+    
+} 
+document.addEventListener("DOMContentLoaded", function(){
+console.log("dom loaded")
+})
